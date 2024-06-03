0e37e71e23b7742bd4c0d9a5cad3541cb14ebf52
Janice-M <janiceink001@gmail.com>
2024-06-03 21:14:03 +0300

    lewis hamilton picture

--- App.js
+++ App.js
@@ -2,6 +2,20 @@ function Profile() {
   return (
     <img
     src="https://imgur.com/a/xu2mpA4"
-    alt="Lewis Hamilton"/>
-  )
+    alt="Lewis Hamilton"
+    />
+  );
+}
+
+export default function Gallery(){
+  return(
+    <section>
+      <h1>The Best Formula One Driver</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+      <Profile />
+
+    </section>
+  );
 }
