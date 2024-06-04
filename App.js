32e3c569de1f45782d8149f3e0f74c1216e51ea1
Janice-M <janiceink001@gmail.com>
2024-06-04 12:22:47 +0300

    importing and exporting components

--- App.js
+++ App.js
@@ -1,24 +1,9 @@
 import Gallery from "./gallery";
 
-function Profile() {
-  return (
-    <img
-    src="assets/lewishamilton.jpeg"
-    alt="Lewis Hamilton"
-    />
-  );
-}
 
-export default function Gallery(){
+export default function App(){
   return(
-    <section>
-      <h1>The Best Formula One Driver</h1>
-      <Profile />
-      <Profile />
-      <p>Lewis Hamilton is a phenomenal Driver </p>
-      
-      
-
-    </section>
+    <Gallery />
+    
   );
 }
