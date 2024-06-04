b98428d39ecce5d23ce12e7c3a10e1f81103f346
Janice-M <janiceink001@gmail.com>
2024-06-04 12:19:12 +0300

    importing and exporting components

--- gallery.js
+++ gallery.js
@@ -1,8 +1,13 @@
-export default function Profile (){
+
+function Profile (){
     return(
         <img
         src="assets/lewishamilton.jpeg"
         alt="Lewis Hamilton"
         />
     );
+}
+
+export default function Gallery(){
+    
 }
